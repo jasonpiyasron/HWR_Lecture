c0fd7a29038d2380d1abcad7841d528c5d38e8af
Stefan Reschke <stefan.reschke@outlook.de>
2023-04-15 21:27:10 +0200

    Add players, because they are required for playing hands

---
+++ src/test/java/hwr/oop/poker/IdentifiablePlayerTest.java
@@ -0,0 +1,34 @@
+package hwr.oop.poker;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Provide Players that are identifiable by a unique name")
+class IdentifiablePlayerTest {
+
+    @Test
+    @DisplayName("Players with different id (1 and 2), are not equal")
+    void twoPlayers_DifferentIdentifier_AreNotEqual() {
+        final Player firstPlayer = new Player("1");
+        final Player secondPlayer = new Player("2");
+        assertThat(firstPlayer).isNotEqualTo(secondPlayer);
+    }
+
+    @Test
+    @DisplayName("Players with same id (both 1), are not equal")
+    void twoPlayers_SameIdentifier_AreEqual() {
+        final Player firstPlayer = new Player("1");
+        final Player secondPlayer = new Player("1");
+        assertThat(firstPlayer).isEqualTo(secondPlayer);
+    }
+
+    @Test
+    @DisplayName("#toString, Player 1337, should contain 1337")
+    void toString_ContainsPlayerId() {
+        final Player player = new Player("1337");
+        final String toString = player.toString();
+        assertThat(toString).contains("1337");
+    }
+}
